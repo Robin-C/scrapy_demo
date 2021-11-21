47b236da0d96eda0c9e5df84fd91ccff53dedee6
Robin C <r.chesne@gmail.com>
2021-11-21 22:13:12 +0100

    racecard and runner list done

---
+++ pmu/pmu/spiders/pmu.py
@@ -0,0 +1,38 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "races"
+
+    def start_requests(self):
+        urls = [
+             'https://horseraces.pmu.fr/racecards'
+            #'http://quotes.toscrape.com/page/2/'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        meetings = response.css('div.meeting.clearfix')
+        for meeting in meetings:
+          yield {
+              'meeting': meeting.css('h3.coursename::text').get(),
+              'races': meeting.css('span.racename::text').getall(),
+              'heures': meeting.css('span.race-time::text').getall()
+          }
+
+        races = response.css('ol.meeting li.result')
+        for race in races:
+              url = race.css('a::attr(href)').get()
+              full_url = response.urljoin(url)
+              race = race.css('span.racename::text').get()
+              yield scrapy.Request(full_url, callback=self.parse_runners, meta={'race': race})
+
+    def parse_runners(self, response):
+        runners = response.css('a.horse-name')
+        for runner in runners:
+            yield {
+                'race': response.meta['race'],
+                'horse_name': runner.css('a.horse-name::text').getall()
+            }
+
